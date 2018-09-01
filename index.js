f258860b3f5d0bbab39fc646a2664f746fd63b9a
jill zhang <talktojillzhang@gmail.com>
2018-09-01 06:34:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -47,9 +47,7 @@ function total() {
 function removeFromCart(item) {
   for (var i =0 ; i<cart.length;i++){
     if (item == cart[i].itemName){
-      cart =  [...cart.splice(0,i-1),...cart.splice(i+1,cart.length-1)]
-      console.log(cart.splice(0,i-1))
-      console.log(cart.splice(i+1,cart.length-1))
+      cart.splice(0,i-1),...cart.splice(i+1,cart.length-1)]
       return cart
     }
   }
