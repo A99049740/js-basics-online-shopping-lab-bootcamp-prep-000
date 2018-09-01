5ad3b08ce7de8a27fa6f10ac55e8387ba165a8eb
jill zhang <talktojillzhang@gmail.com>
2018-09-01 06:40:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber=='' || cart.empty){
+  if (cardNumber==''){
     return "Sorry, we don't have a credit card on file for you."
   }
   else{
