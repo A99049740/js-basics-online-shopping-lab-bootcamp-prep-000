36961d8623542db85a2a791469fd66d6c460aab3
jill zhang <talktojillzhang@gmail.com>
2018-09-01 06:37:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -60,9 +60,9 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   }
   else{
-    var total = total()
+    var totalValue = total()
     cart =[]
-    return total
+    return totalValue
   }
   // write your code here
 }
